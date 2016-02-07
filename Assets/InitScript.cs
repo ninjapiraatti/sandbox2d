dd58030caf0785c294de3ead3350b45e5db83511
Ninjapiraatti <tuoppil@gmail.com>
2016-02-07 15:37:04 +0200

    Random movement.

---
+++ Assets/InitScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitScript : MonoBehaviour {
+
+	public GameObject hahmo;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hello World!");
+        for (int y = -2; y < 1; y++) {
+            for (int x = -2; x < 1; x++) {
+                //Instantiate(hahmo, new Vector2 (x, y), Quaternion.identity);
+				GameObject hahmoInstance = (GameObject)Instantiate(hahmo, new Vector2 (x, y), Quaternion.identity);
+            }
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/hahmo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hahmo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hahmo luotu");
+		movement();
+	}
+
+	// Update is called once per frame
+	float directionX = 0.3F;
+	float directionY = 0.3F;
+	bool moving = true;
+	float randomBoolean = Random.Range(0.1F,7.0F);
+
+	void Update () {
+		if (!moving) {
+
+		} else {
+			transform.Translate((directionX * Time.deltaTime), (directionY * Time.deltaTime),0);
+		}
+	}
+	void changeDirection() {
+		Debug.Log (randomBoolean);
+		randomBoolean = Random.Range(0.0F,10.0F);
+		if(randomBoolean < 5.0F) {
+			moving = false;
+		} else {
+			moving = true;
+			directionY = Random.Range(-0.3F,0.3F);
+			directionX = Random.Range(-0.3F,0.3F);
+		}
+	}
+	void movement() {
+		InvokeRepeating("changeDirection", 0, randomBoolean);
+	}
+
+}
